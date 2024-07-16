0ec971597716cfd5d24af28ff1d654489ab6289a
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-07-16 17:59:55 +0000

    Exercises Done

--- exercises/01.3-building-a-layout/solution.hide.jsx
+++ exercises/01.3-building-a-layout/solution.hide.jsx
@@ -5,7 +5,10 @@ const data = {
 	image: "../../.learn/assets/Dylan.png?raw=true",
 	cardTitle: "Bob Dylan",
 	cardDescription:
-		"Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
+		"Bob Dylan (born Robert Allen Zimmerman, May 24, 1941)
+		 is an American singer-songwriter, author, 
+		 and artist who has been an influential figure in popular
+		  music and culture for more than five decades.",
 	button: {
 		url: "https://en.wikipedia.org/wiki/Bob_Dylan",
 		label: "Go to wikipedia",
--- exercises/01.4-building-html-from-arrays/app.jsx
+++ exercises/01.4-building-html-from-arrays/app.jsx
@@ -2,8 +2,42 @@ import React from "react"; // Main React.js library
 import ReactDOM from "react-dom"; // We use ReactDOM to render into the DOM
 
 // Only update the value of this array
-const navlinkItems = [];
+const navlinkItems = [
+	{
+		id: 0,
+		contentLi: (
+			<li className="nav-item">
+				<a className="nav-link" href="#">
+					Link to google.com
+				</a>
+			</li>
+		),
+	},
+	{
+		id: 1,
+		contentLi: (
+			<li className="nav-item">
+				<a className="nav-link" href="#">
+					Link to facebook.com
+				</a>
+			</li>
+		),
+	},
+	{
+		id: 2,
+		contentLi: (
+			<li className="nav-item">
+				<a className="nav-link" href="#">
+					Link to amazon.com
+				</a>
+			</li>
+		),
+	},
+];
+let mapeadoNavLinkItems = navlinkItems.map((navLinkItem) => {
+	return navLinkItem.contentLi;
+});
 
-const content = <ul className="nav">{navlinkItems}</ul>;
+const content = <ul className="nav">{mapeadoNavLinkItems}</ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
